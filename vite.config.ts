6fd75aa5f7122a6d827d55eabdd16905da179bb8
swalihkolakkadan <swalihkolakkadan@gmail.com>
2026-03-28 10:50:39 +0530

    fix: Added environment for running unit test

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import path from "node:path";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  test: {
+    environment: "happy-dom",
+    setupFiles: ["./src/test/setup.ts"],
+    globals: true,
+  },
   plugins: [
     babel({
       plugins: ["babel-plugin-react-compiler"],
